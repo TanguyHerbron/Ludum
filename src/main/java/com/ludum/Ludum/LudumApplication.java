c282a8fdf038f7eab4d11661a665da76a99b4807
Tanguy Herbron <tanguy.herbron@outlook.com>
2019-01-10 17:35:14 +0100

    Push test for untracked files

--- src/main/java/com/ludum/Ludum/LudumApplication.java
+++ src/main/java/com/ludum/Ludum/LudumApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LudumApplication {
 
+	//Something to allow a push
 	public static void main(String[] args) {
 		SpringApplication.run(LudumApplication.class, args);
 	}
