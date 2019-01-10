802dc1aeb603273b59eca230b1e822acb5f85a3c
Tanguy Herbron <tanguy.herbron@outlook.com>
2019-01-10 16:44:25 +0100

    First commit

---
+++ src/main/java/com/ludum/Ludum/LudumApplication.java
@@ -0,0 +1,12 @@
+package com.ludum.Ludum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LudumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LudumApplication.class, args);
+	}
+}
