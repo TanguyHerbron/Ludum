4a177ca68d2d97feaba94195a2dc67891d3771c8
Tanguy Herbron <tanguy.herbron@outlook.com>
2019-01-14 12:33:05 +0100

    Match code preparation
    Add Game Repository
    Add Participation Repository
    Some not useful for now yet useful annotations for later

--- src/main/java/com/ludum/Ludum/controller/MatchController.java
+++ src/main/java/com/ludum/Ludum/controller/MatchController.java
@@ -1,15 +1,23 @@
 package com.ludum.Ludum.controller;
 
 import com.ludum.Ludum.model.Match;
+import com.ludum.Ludum.model.TournamentRepository;
+import com.ludum.Ludum.repository.MatchRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class MatchController {
+
+    @Autowired
+    TournamentRepository tournamentRepository;
+
+    @Autowired
+    MatchRepository matchRepository;
 
     @GetMapping("/createMatch")
     public String createMatch()
@@ -21,12 +29,23 @@ public class MatchController {
     public String addMatch(Model model, @ModelAttribute("createMatchFrom") Match match)
     {
         model.addAttribute("match", match);
+
+        /*
+        Prepare the associations between the tournament table and the match table
+        Could need an autowired attribut for the inbetween table
+         */
+
         return "match-page";
     }
 
     @GetMapping("/displayMatchsFor")
-    public String displayMatchs()
+    public String displayMatchs(@RequestParam(name="tournamentId")Long tournamentId)
     {
+        if(tournamentRepository.findById(tournamentId).isPresent())
+        {
+
+        }
+
         return "listMatchs";
     }
 }
