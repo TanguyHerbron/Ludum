ee6067113d16b29071494cafdf6ff5aa03499ee8
Gabriel Lemée <gabigabdu72@hotmail.com>
2019-01-14 10:26:59 +0100

    Fix css

--- src/main/java/com/ludum/Ludum/model/Person.java
+++ src/main/java/com/ludum/Ludum/model/Person.java
@@ -1,4 +1,31 @@
 package com.ludum.Ludum.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Person {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
