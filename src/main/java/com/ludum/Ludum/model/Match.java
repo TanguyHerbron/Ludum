c3ed9df708cbaa985f35ec5edcfbfc68182ec5d9
Gabriel Lemée <gabigabdu72@hotmail.com>
2019-01-15 10:56:20 +0100

    FIX  MATCH SH*T

--- src/main/java/com/ludum/Ludum/model/Match.java
+++ src/main/java/com/ludum/Ludum/model/Match.java
@@ -1,4 +1,17 @@
 package com.ludum.Ludum.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+@Entity
 public class Match {
+    @Id
+    private Long idMatch;
+
+    private Date date;
+
+    private String location;
+
+    private Long idTournoi;
 }
