e0faf233db2ce3a4a0494b0420131ecbacbf5541
Tanguy Herbron <tanguy.herbron@outlook.com>
2019-01-16 09:53:40 +0100

    Fix repo package

--- src/main/java/com/ludum/Ludum/controller/TournamentController.java
+++ src/main/java/com/ludum/Ludum/controller/TournamentController.java
@@ -1,7 +1,7 @@
 package com.ludum.Ludum.controller;
 
 import com.ludum.Ludum.model.Tournament;
-import com.ludum.Ludum.model.TournamentRepository;
+import com.ludum.Ludum.repository.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
