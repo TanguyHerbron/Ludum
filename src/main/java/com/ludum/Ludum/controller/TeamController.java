ca8c7cc177759bf9ea5afbeb7851da4716061cd8
Gabriel Lemée <gabigabdu72@hotmail.com>
2019-01-16 10:01:45 +0100

    Create team fixed

--- src/main/java/com/ludum/Ludum/controller/TeamController.java
+++ src/main/java/com/ludum/Ludum/controller/TeamController.java
@@ -1,12 +1,44 @@
 package com.ludum.Ludum.controller;
 
+import com.ludum.Ludum.model.Team;
+import com.ludum.Ludum.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class TeamController {
+    @Autowired
+    TeamRepository teamRepository;
+
+    @GetMapping("/list-team-page")
+    public String listTeamPage(){
+        return "list-team-page";
+    }
+
     @GetMapping("/team-page")
     public String teamPage(){
+        return "team-page";
+    }
+
+    @GetMapping("/create-team-page")
+    public String createTeamPage(Model model){
+        model.addAttribute("team", new Team());
+        return "create-team-page";
+    }
+
+    @PostMapping("/create-team-page")
+    public String createTeamSubmit(Model model, @ModelAttribute("teamForm") Team team) {
+        System.out.println(team.getName());
+        teamRepository.save(team);
+        return "team-page";
+    }
+
+    @RequestMapping(value = "/delete-team", method = {RequestMethod.POST})
+    public String deleteTeam(Model model, @ModelAttribute("deleteTeamForm") Team newTeam){
+
+        model.addAttribute("team", newTeam);
         return "team-page";
     }
 }
--- src/main/java/com/ludum/Ludum/model/Team.java
+++ src/main/java/com/ludum/Ludum/model/Team.java
@@ -1,4 +1,40 @@
 package com.ludum.Ludum.model;
 
+import javax.persistence.*;
+
+@Entity
 public class Team {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "city")
+    private String city;
+
+    public Team(){
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() { return city; }
+
+    public void setCity(String city) { this.city = city; }
 }
